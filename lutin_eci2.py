db109fe7a520ffb23e16de3ebbaeec03c61dc190
Edouard DUPIN <yui.heero@gmail.com>
2016-10-04 23:41:29 +0200

    [DEV] Update new lutin 2.2.0 (no legacy support)

--- lutin_eci2.py
+++ lutin_eci2.py
@@ -3,13 +3,24 @@ import lutin.module as module
 import lutin.tools as tools
 import os
 
+def get_type():
+	return "LIBRARY"
+
 def get_desc():
 	return "Ewol C Interpreter"
 
-def create(target, module_name):
-	myModule = module.Module(__file__, module_name, 'BINARY')
-	myModule.add_extra_compile_flags()
-	myModule.add_src_file([
+def get_licence():
+	return "APACHE-2"
+
+def get_compagny_type():
+	return "com"
+
+def get_compagny_name():
+	return "atria-soft"
+
+def configure(target, my_module):
+	my_module.add_extra_flags()
+	my_module.add_src_file([
 		'eci/eci.cpp',
 		'eci/Lexer.cpp',
 		'eci/debug.cpp',
@@ -25,7 +36,7 @@ def create(target, module_name):
 		'eci/lang/ParserCpp.cpp',
 		'eci/lang/ParserJS.cpp'
 		])
-	myModule.add_export_path(tools.get_current_path(__file__))
-	myModule.add_module_depend('etk')
-	return myModule
+	my_module.add_path(".", export=True)
+	my_module.add_depend('etk')
+	return True
 
